542694e2cb18f929b432733a2420b533a4e2381a
dctyler <daniel@tyleruk.com>
2022-12-20 01:31:01 +0000

    Added failed attempt 1 at day16.

--- Solutions/day16/Valve.py
+++ Solutions/day16/Valve.py
@@ -1,4 +1,5 @@
 import re
+import copy
 
 class Valve:
     def __init__(self, id, flow_rate):
@@ -6,14 +7,22 @@ class Valve:
         self.id = id
         self.flow_rate = flow_rate
         self.connections = []  # List of valves connecting this one
+        self.connection_ids = []
 
     # Connects valves to another one. (Chose to not bother making it both ways, just because I'm lazy and the input is sanitized
     def add_connection(self, valve):
         self.connections.append(valve)
+        self.connection_ids.append(valve.id)
         # valve.connections.add(self.connections)
 
     def __str__(self) -> str:
         return self.id + " " + str(self.flow_rate) + " " + self.connections_string()
+
+    def __eq__(self, o: object) -> bool:
+        return self.id == o.id
+
+    def __hash__(self) -> int:
+        return hash(self.id)
 
     def connections_string(self):
         string = "("
@@ -23,6 +32,7 @@ class Valve:
         string += ")"
 
         return re.sub(",\)",")", re.sub(" ", ",", string))
+
 
 class Network:
     def __init__(self, input_string):
@@ -52,7 +62,82 @@ class Network:
                 tunnel_destination = self.get_valve_by_id(tunnel_id)
                 valve.add_connection(tunnel_destination)
 
-    def get_valve_by_id(self, id):
+    def get_valve_by_id(self, valve_id):
         for valve in self.valves:
-            if valve.id == id:
+            if valve.id == valve_id:
                 return valve
+
+
+class NetworkStatus:
+    def __init__(self, network: Network, age, total_pressure, unit_pressure, location, open_valves, previous_location):
+        super().__init__()
+        self.network = network
+
+        self.age = age
+        self.total_pressure = total_pressure
+        self.unit_pressure = unit_pressure
+        self.location = location
+        self.open_valves = open_valves
+        self.previous_location = previous_location
+
+    def __str__(self) -> str:
+        valves_string = "OpenValves: "
+        for valve in self.open_valves:
+            valves_string += valve.id
+            valves_string += ","
+        return "Age: " + str(self.age) + " TotPressure: " + str(self.total_pressure) + " Location: " + str(self.location.id) + " " + valves_string
+
+    def __eq__(self, o: object) -> bool:
+        return self.total_pressure == o.total_pressure and self.location.id == o.location.id and self.open_valves == o.open_valves
+
+    def __hash__(self) -> int:
+        return hash(self.age) + hash(self.total_pressure) + hash(self.location.id) + hash(tuple(self.open_valves))
+
+    def open_valves_string(self):
+        valve_string = ""
+        for valve in self.open_valves:
+            valve_string += valve.id
+        return valve_string
+
+    def add_pressure(self):
+        self.total_pressure += self.unit_pressure
+
+    def get_step_options(self):
+        step_options = []
+        if not self.is_location_valve_open() and self.location.flow_rate > 0:
+            step_options = ["Open"]
+        for connection in self.location.connection_ids:
+            if self.previous_location.id != connection:
+                step_options.append(connection)
+        return step_options
+
+    def is_location_valve_open(self):
+        for valve in self.open_valves:
+            if valve.id == self.location.id:
+                return True
+        return False
+
+    def create_next_steps(self):
+        step_options = self.get_step_options()
+
+        self.add_pressure()
+
+        new_statuses = []
+        new_age = self.age + 1
+
+        for option in step_options:
+
+            if option == "Open":
+                new_open_valves = [self.location]
+                new_open_valves.extend(self.open_valves)
+                self.unit_pressure += self.location.flow_rate
+                new_statuses.append(NetworkStatus(self.network, new_age,
+                                                  self.total_pressure, self.unit_pressure, copy.deepcopy(self.location),
+                                                  copy.deepcopy(new_open_valves), copy.deepcopy(self.location), ))
+
+            else:
+                new_location = self.network.get_valve_by_id(option)
+                new_statuses.append(NetworkStatus(self.network, new_age, self.total_pressure, self.unit_pressure, new_location,
+                                                  copy.deepcopy(self.open_valves), copy.deepcopy(self.location)))
+
+        return new_statuses
--- Solutions/day16/day16.py
+++ Solutions/day16/day16.py
@@ -1,8 +1,61 @@
-from Solutions.day16.Valve import Network
+from Solutions.day16.Valve import NetworkStatus, Network
 
 f = open("../../Inputs/day16/test_data.dat", "r")
 input = f.read().splitlines()
 
 network = Network(input)
 
-belb = 1
+initial_network_state = NetworkStatus(network, 0, 0, 0, network.get_valve_by_id("AA"), set(), network.get_valve_by_id("AA"))
+
+network_states = [initial_network_state]
+
+
+def take_step(network_states):
+    new_network_states = set()
+    for state in network_states:
+        this_new_states = state.create_next_steps()
+        for new_state in this_new_states:
+            new_network_states.add(new_state)
+    return new_network_states
+
+def prune_states(network_states):
+
+    tot_pressure_map = {}
+    to_pop = []
+
+    for state in network_states:
+        state_string = state.location.id + "-" + state.open_valves_string()
+        if tot_pressure_map.get(state_string) != None:
+            if tot_pressure_map.get(state_string) < state.total_pressure:
+                tot_pressure_map.update({state_string: state.total_pressure})
+        else:
+            tot_pressure_map.update({state_string: state.total_pressure})
+
+    for state in set(network_states):
+        state_string = state.location.id + "-" + state.open_valves_string()
+        if tot_pressure_map[state_string] > state.total_pressure:
+            network_states.remove(state)
+
+    return network_states
+
+
+
+
+# network_states = take_step(network_states)
+# network_states = take_step(network_states)
+# network_states = take_step(network_states)
+
+for i in range(0, 30):
+    print("Taking step ", i)
+    print("There are currently: ", len(network_states), " network statues")
+    network_states = take_step(network_states)
+    pruned_network_states = prune_states(network_states)
+    belb = 1
+
+max_pressure = 0
+
+for status in network_states:
+    if status.total_pressure > max_pressure:
+        max_pressure = status.total_pressure
+
+print("Max total pressure is: ", max_pressure)
