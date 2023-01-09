b535395a307ba92c7ea2032fa5762e84eb1c92fb
dctyler <daniel@tyleruk.com>
2023-01-09 09:05:24 +0000

    Added optmisation to only allow the building of as many material robots as the max cost of that type. It now runs through the blueprints, but spits out an incorrect answer. (660 too low)

--- Solutions/day19/RobotFactory.py
+++ Solutions/day19/RobotFactory.py
@@ -17,17 +17,25 @@ class RobotFactory:
         self.obsidian_robot_cost = [values[3], values[4], 0]
         self.geode_robot_cost = [values[5], 0, values[6]]
 
+        self.max_ore_cost = max(self.ore_robot_cost[0], self.clay_robot_cost[0], self.obsidian_robot_cost[0], self.geode_robot_cost[0])
+        self.max_clay_cost = max(self.ore_robot_cost[1], self.clay_robot_cost[1], self.obsidian_robot_cost[1],
+                                self.geode_robot_cost[1])
+        self.max_obsidian_cost = max(self.ore_robot_cost[2], self.clay_robot_cost[2], self.obsidian_robot_cost[2],
+                                self.geode_robot_cost[2])
+
     # There are 5 possible options, do nothing, or build each type of robot. Figure out which are possible
     # The options are:
     # 0-4 for do nothing, build ore robot, build clay robot, build obsidian robot, build geode robot
-    def what_robots_can_be_built(self, material_counts):
+    def what_robots_can_be_built(self, material_counts, robot_counts):
         options = [True]
-        options.append(material_counts[0] >= self.ore_robot_cost[0])
-        options.append(material_counts[0] >= self.clay_robot_cost[0])
+        options.append(material_counts[0] >= self.ore_robot_cost[0] and robot_counts[0] <= self.max_ore_cost)
+        options.append(material_counts[0] >= self.clay_robot_cost[0] and robot_counts[1] <= self.max_clay_cost)
         options.append(
-            material_counts[0] >= self.obsidian_robot_cost[0] and material_counts[1] >= self.obsidian_robot_cost[1])
+            material_counts[0] >= self.obsidian_robot_cost[0] and material_counts[1] >= self.obsidian_robot_cost[1] and
+            robot_counts[2] <= self.max_obsidian_cost
+        )
         options.append(
-            material_counts[0] >= self.obsidian_robot_cost[0] and material_counts[2] >= self.geode_robot_cost[2])
+            material_counts[0] >= self.geode_robot_cost[0] and material_counts[2] >= self.geode_robot_cost[2])
         return options
 
 
@@ -45,12 +53,12 @@ class RobotFactoryState:
     # Gather resources
     # Build new robot
     def advance_one_min(self):
-        next_step_options = self.robot_factory.what_robots_can_be_built(self.material_counts)
+        next_step_options = self.robot_factory.what_robots_can_be_built(self.material_counts, self.robot_counts)
         self.gather_resources()
         return self.get_next_min_states(next_step_options)
 
     def gather_resources(self):
-        self.material_counts += self.robot_counts
+        self.material_counts = tuple(map(sum, zip(self.material_counts, self.robot_counts)))
 
     def get_next_min_states(self, next_step_options):
         new_states = []
@@ -66,50 +74,45 @@ class RobotFactoryState:
         if next_step_options[3]:
             new_states.append(RobotFactoryState(self.robot_factory, self.age + 1, self.add_obsidian_robot(),
                                                 self.pay_for_obsidian_robot()))
-        if next_step_options[3]:
+        if next_step_options[4]:
             new_states.append(
                 RobotFactoryState(self.robot_factory, self.age + 1, self.add_geode_robot(), self.pay_for_geode_robot()))
         return new_states
 
     def add_ore_robot(self):
-        return np.array((self.robot_counts[0] + 1, self.robot_counts[1], self.robot_counts[2], self.robot_counts[3]))
+        return (self.robot_counts[0] + 1, self.robot_counts[1], self.robot_counts[2], self.robot_counts[3])
 
     def pay_for_ore_robot(self):
-        return np.array((self.material_counts[0] - self.robot_factory.ore_robot_cost[0], self.material_counts[1], \
-               self.material_counts[2], self.material_counts[3]))
+        return (self.material_counts[0] - self.robot_factory.ore_robot_cost[0], self.material_counts[1], \
+               self.material_counts[2], self.material_counts[3])
 
     def add_clay_robot(self):
-        return np.array((self.robot_counts[0], self.robot_counts[1] + 1, self.robot_counts[2], self.robot_counts[3]))
+        return (self.robot_counts[0], self.robot_counts[1] + 1, self.robot_counts[2], self.robot_counts[3])
 
     def pay_for_clay_robot(self):
-        return np.array((self.material_counts[0] - self.robot_factory.clay_robot_cost[0], self.material_counts[1], \
-               self.material_counts[2], self.material_counts[3]))
+        return (self.material_counts[0] - self.robot_factory.clay_robot_cost[0], self.material_counts[1], \
+               self.material_counts[2], self.material_counts[3])
 
     def add_obsidian_robot(self):
-        return np.array((self.robot_counts[0], self.robot_counts[1], self.robot_counts[2] + 1, self.robot_counts[3]))
+        return (self.robot_counts[0], self.robot_counts[1], self.robot_counts[2] + 1, self.robot_counts[3])
 
     def pay_for_obsidian_robot(self):
-        return np.array((self.material_counts[0] - self.robot_factory.obsidian_robot_cost[0],
+        return (self.material_counts[0] - self.robot_factory.obsidian_robot_cost[0],
                 self.material_counts[1] - self.robot_factory.obsidian_robot_cost[1],
                 self.material_counts[2],
-                self.material_counts[3]))
+                self.material_counts[3])
 
     def add_geode_robot(self):
-        return np.array((self.robot_counts[0], self.robot_counts[1], self.robot_counts[2], self.robot_counts[3] + 1))
+        return (self.robot_counts[0], self.robot_counts[1], self.robot_counts[2], self.robot_counts[3] + 1)
 
     def pay_for_geode_robot(self):
-        return np.array((self.material_counts[0] - self.robot_factory.geode_robot_cost[0],
+        return (self.material_counts[0] - self.robot_factory.geode_robot_cost[0],
                 self.material_counts[1],
                 self.material_counts[2] - self.robot_factory.geode_robot_cost[2],
-                self.material_counts[3]))
+                self.material_counts[3])
 
     # A state is deemed better if it has more than or equal to in each material and robot type
     def is_input_state_better(self, input_state):
-
-        # # self.robot_counts.
-        # if min(self.robot_counts <= input_state.robot_counts) and min(self.material_counts <= input_state.material_counts):
-        #     return True
-        # return False
 
         if (self.robot_counts[0] <= input_state.robot_counts[0] and
                 self.robot_counts[1] <= input_state.robot_counts[1] and
@@ -121,6 +124,34 @@ class RobotFactoryState:
                 self.material_counts[3] <= input_state.material_counts[3]):
             return True
         return False
+
+    # Assuming you build one geode robot per min from this point, how many geodes would you mine?
+    def theoretical_max_geodes(self, time_remaining, recurse=False):
+
+        next_options = self.robot_factory.what_robots_can_be_built(self.material_counts, self.robot_counts)
+
+        # I think the recurse is just a plain bad idea
+        if recurse and time_remaining > 1:
+            next_steps = self.get_next_min_states(next_options)
+            theoretical_max_geodes = 0
+            for state in next_steps:
+                if state.theoretical_max_geodes(time_remaining-1, False) > theoretical_max_geodes:
+                    theoretical_max_geodes = state.theoretical_max_geodes(time_remaining-1, False)
+            return theoretical_max_geodes
+
+        else:
+            can_geode_robot_be_built_in_next_min = next_options[4]
+
+            psudo_time_remaining = time_remaining
+            if not can_geode_robot_be_built_in_next_min:
+                psudo_time_remaining -= 1
+
+            return (self.material_counts[3] +
+                    self.robot_counts[3] * time_remaining +
+                    int((psudo_time_remaining - 1) * (psudo_time_remaining - 2) / 2))
+
+
+
 
     def __str__(self) -> str:
         return "Age: " + str(self.age) + " Mat counts: " + str(self.material_counts) + " Robot counts: " + str(
--- Solutions/day19/day19.py
+++ Solutions/day19/day19.py
@@ -3,7 +3,7 @@ import re
 
 from Solutions.day19.RobotFactory import RobotFactory, RobotFactoryState
 
-f = open("../../Inputs/day19/test_data.dat", "r")
+f = open("../../Inputs/day19/part1.dat", "r")
 input = f.read().splitlines()
 
 test = re.findall('\d+', input[0])
@@ -13,45 +13,127 @@ robot_factories = []
 for line in input:
     robot_factories.append(RobotFactory(line))
 
+def is_robot_count_b_better_than_a(a,b):
+    # return a != b and (a[0] <= b[0] and
+    #             a[1] <= b[1] and
+    #             a[2] <= b[2] and
+    #             a[3] <= b[3])
+    return (a[0] <= b[0] and
+                a[1] <= b[1] and
+                a[2] <= b[2] and
+                a[3] <= b[3])
 
-init_state = RobotFactoryState(robot_factories[0], 0, np.array((1, 0, 0, 0)), np.array((0, 0, 0, 0)))
+def prune_states_that_cant_catch_max_geode_count(states, time_remaining):
+    # Step 0.5, see which states have no chance of catching up.
 
-time_passed = 0
-latest_states = [init_state]
+    max_geodes = 0
+    for state in states:
+        if state.material_counts[3] + state.robot_counts[3] * time_remaining > max_geodes:
+            max_geodes = state.material_counts[3] + state.robot_counts[3] * time_remaining
 
-def prune_states(states):
+    print("Starting with ", len(states), " states for catch_max_geode pruning, time_remaining = ", time_remaining, ", max_geodes = ", max_geodes)
 
-    print("Pruning states")
+    if max_geodes == 0:
+        print("No geodes found")
+        return states
+
     to_remove = set()
 
-    for state_check_index in range(0, len(states)):
-        for loop_state in range(0, len(states)):
-            if loop_state == state_check_index:
-                pass
-            elif states[state_check_index].is_input_state_better(states[loop_state]):
-                to_remove.add(state_check_index)
-                break
+    for state_index in range(0, len(states)):
+        if states[state_index].theoretical_max_geodes(time_remaining) < max_geodes:
+            to_remove.add(state_index)
 
     if len(to_remove) > 0:
         print("Removing ", len(to_remove), " states")
         to_remove = sorted(list(to_remove))
         for loop_state in reversed(to_remove):
             del states[loop_state]
-    print("Done pruning states")
+    print("Ended with ", len(states), " states for catch_max_geode pruning")
+
     return states
 
 
+def prune_states(states, time_remaining):
+
+    states = prune_states_that_cant_catch_max_geode_count(states, time_remaining)
+
+    print("Pruning states")
+    to_remove_attempt_2 = set()
+
+    # Step one, build a dictionary of all robot counts, each will contain a list of material counts
+    state_dictionary = dict()
+
+    for state in states:
+        if state_dictionary.get(state.robot_counts) is None:
+            state_dictionary[state.robot_counts] = set()
+        state_dictionary[state.robot_counts].add(state.material_counts)
+
+    # Step one point 5, loop through dictionary and kick out already inferior cases
+    # for state in state_dictionary:
+
+    # Step two, loop through all states and only check the appropriate dictionary element
+    for state_check_index in range(0, len(states)):
+        for key in state_dictionary.keys():
+            if is_robot_count_b_better_than_a(states[state_check_index].robot_counts, key):
+                for material_count in state_dictionary[key]:
+                    if is_robot_count_b_better_than_a(states[state_check_index].material_counts, material_count):
+                        if states[state_check_index].material_counts == material_count and states[state_check_index].robot_counts == key:
+                            pass
+                        else:
+                            to_remove_attempt_2.add(state_check_index)
+                        break
+
+    # Step three, remove repeated elements?
+    to_remove = to_remove_attempt_2
+    # # Slow and simple pruning method
+    # print("Pruning states")
+    # to_remove = set()
+    #
+    # for state_check_index in range(0, len(states)):
+    #     for loop_state in range(0, len(states)):
+    #         if loop_state == state_check_index:
+    #             pass
+    #         elif states[state_check_index].is_input_state_better(states[loop_state]):
+    #             to_remove.add(state_check_index)
+    #             break
+
+    # Now actually remove them
+    if len(to_remove) > 0:
+        print("Removing ", len(to_remove), " states")
+        to_remove = sorted(list(to_remove))
+        for loop_state in reversed(to_remove):
+            del states[loop_state]
+
+    print("Done Pruning. Pruning removed: ", len(to_remove), " states to leave", len(states), "states")
+
+    return states
 
 
+def find_max_geodes_of_blueprint(init_state):
+    time_passed = 0
+    latest_states = [init_state]
+    max_time = 24
+    while time_passed < max_time:
+        new_latest_states = []
+        for state in latest_states:
+            new_latest_states += state.advance_one_min()
+        time_passed += 1
+        latest_states = new_latest_states
+        latest_states = prune_states(latest_states, max_time - time_passed)
+        print("After ", time_passed, " mins, there are: ", len(latest_states), " states")
+
+    print("Max geode count is:", latest_states[0].material_counts[3])
+    return latest_states[0].material_counts[3]
 
 
-while time_passed < 24:
-    new_latest_states = []
-    for state in latest_states:
-        new_latest_states += state.advance_one_min()
-    time_passed += 1
-    latest_states = new_latest_states
-    latest_states = prune_states(latest_states)
-    print("After ", time_passed, " mins, there are: ", len(latest_states), " states")
+quality_levels = []
+quality_level_sum = 0
 
-belb = 1
+for blueprint in robot_factories:
+    print("Running blueprint number:", blueprint.id)
+    init_state = RobotFactoryState(blueprint, 0, (1, 0, 0, 0), (0, 0, 0, 0))
+    max_geodes = find_max_geodes_of_blueprint(init_state)
+    quality_levels.append(max_geodes * blueprint.id)
+    quality_level_sum += max_geodes * blueprint.id
+
+print("Total quality level: ", quality_level_sum)
