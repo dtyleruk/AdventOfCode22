15cca24d855c44d2870265ad55ede295664d0a48
dctyler <daniel@tyleruk.com>
2023-01-12 01:11:37 +0000

    A lot of fun, but sadly the part2 example works it doesn't with the real input.

--- Solutions/day21/YellingMonkey.py
+++ Solutions/day21/YellingMonkey.py
@@ -19,6 +19,12 @@ class YellingMonkey:
             self.operation = None
             self.parent2 = None
 
+    def get_parent_n_value(self, n):
+        if n == 1:
+            return self.monkey_dict[self.parent1].value
+        else:
+            return self.monkey_dict[self.parent2].value
+
     def calc_value(self):
         if self.value is not None:
             return self.value
@@ -31,6 +37,12 @@ class YellingMonkey:
         self.value = self.perform_operation()
 
     def perform_operation(self):
+
+        if type(self.monkey_dict[self.parent1].value) == list:
+            return self.add_operation_string(1)
+        if type(self.monkey_dict[self.parent2].value) == list:
+            return self.add_operation_string(2)
+
         value = self.monkey_dict[self.parent1].value
         if self.operation == "+":
             return value + self.monkey_dict[self.parent2].value
@@ -39,9 +51,16 @@ class YellingMonkey:
         elif self.operation == "*":
             return value * self.monkey_dict[self.parent2].value
         elif self.operation == "/":
-            return value / self.monkey_dict[self.parent2].value
+            return value // self.monkey_dict[self.parent2].value
         else:
             raise Exception
+
+    def add_operation_string(self, which_parent_is_list):
+        list_of_operations = self.get_parent_n_value(which_parent_is_list)
+        non_annoying_parent_value = self.get_parent_n_value(3 - which_parent_is_list)
+        list_of_operations.append((self.operation, non_annoying_parent_value))
+        return list_of_operations
+
 
     def __str__(self) -> str:
         string_val = self.name + " Value: " + str(self.value)
--- Solutions/day21/day21.py
+++ Solutions/day21/day21.py
@@ -3,13 +3,52 @@ from Solutions.day21.YellingMonkey import YellingMonkey
 f = open("../../Inputs/day21/part1.dat", "r")
 input = f.read().splitlines()
 
-monkey_dict = dict()
 
-for input_line in input:
-    monkey = YellingMonkey(input_line, monkey_dict)
-    monkey_dict[monkey.name] = monkey
+def make_monkey_dict(input):
+    monkey_dict = dict()
+    for input_line in input:
+        monkey = YellingMonkey(input_line, monkey_dict)
+        monkey_dict[monkey.name] = monkey
+    return monkey_dict
 
+
+# Part 1
+monkey_dict = make_monkey_dict(input)
 monkey_dict["root"].calc_value()
 print("Root's number is: ", monkey_dict["root"].value)
 
+# Part 2
+# Edit inputs
+monkey_dict = make_monkey_dict(input)
+monkey_dict["root"].operation = "="
+monkey_dict["humn"].value = ["x"]
 
+monkey_dict["root"].calc_value()
+
+
+def perform_reverse_operation(RHS, operation):
+
+    if operation[0] == "+":
+        return RHS - operation[1]
+    if operation[0] == "-":
+        return RHS + operation[1]
+    if operation[0] == "*":
+        return RHS // operation[1]
+    if operation[0] == "/":
+        return RHS * operation[1]
+    raise Exception
+
+
+def unpick_operation(operation_list):
+    RHS = operation_list.pop(-1)[1]
+    while len(operation_list) > 1:
+        print("RHS:", RHS, " Operation:", operation_list[-1])
+        RHS = perform_reverse_operation(RHS, operation_list.pop(-1))
+
+
+    return RHS
+
+
+
+value_to_yell = unpick_operation(monkey_dict["root"].value)
+print("Value for human to yell is:", value_to_yell)
